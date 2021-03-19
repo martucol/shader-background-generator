3f9b77f45b497cd90883cd4e32ed84e858562d6f
Martina Colmenero <martinacolmenero@nayra.coop>
2021-03-19 12:00:04 -0300

    add parameters

--- src/shader/add-noise-layer.glsl
+++ src/shader/add-noise-layer.glsl
@@ -1,8 +1,11 @@
 vec3 addnoiselayer (vec3 layer, vec3 noise) {
+    // PARAMETERS //
+    float noiseamount = 0.65; // between 0.0 and 0.80
+    float mixamount = 0.2;
 
-    float amount = 0.65; // parameter: between 0.0 and 0.80
-    vec3 noisescreen = mix(noise, layer, length(normalize(noise))+ amount);
-    vec3 final = mix(layer, noisescreen, 0.2);
+    vec3 noisescreen = mix(noise, layer, length(normalize(noise))+ noiseamount);
+
+    vec3 final = mix(layer, noisescreen, mixamount);
     return final;
 }
 
